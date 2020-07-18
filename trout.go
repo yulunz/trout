aa17c215514fff51588f2661de64c96f8fd2c68e
Yulun Zeng <hi@yulunzeng.me>
2020-07-17 22:43:27 -0500

    initial working site

---
+++ trout.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "html/template"
+    "log"
+    "net/http"
+    "regexp"
+)
+
+var templates = template.Must(template.ParseFiles("trout.html"))
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+    err := templates.Execute(w, struct{}{})
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+var staticPath = regexp.MustCompile("^/static/([a-zA-Z0-9_.]+)$")
+
+func StaticHandler(w http.ResponseWriter, r *http.Request) {
+	fileName := staticPath.FindStringSubmatch(r.URL.Path)
+	if fileName == nil || len(fileName) <= 1 || (fileName[1] != "cat_fish.jpg" && fileName[1] != "favicon.ico"){
+		http.NotFound(w, r)
+		return
+	}
+	http.ServeFile(w, r, fileName[1])
+}
+
+func main() {
+    http.HandleFunc("/", IndexHandler)
+    http.HandleFunc("/static/", StaticHandler)
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
