2538f9d705dd638482cf19f29b912b987cf41636
Yulun Zeng <yz5454@gmail.com>
2026-01-04 16:24:50 +0100

    fix build

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 workingDir="$(dirname $0)"
 mkdir -p $workingDir/dist/static
 
-cp cat_fish.jpg $workingDir/dist/static
+cp cat.jpg $workingDir/dist/static
 cp favicon.ico $workingDir/dist/static
 cp trout.html $workingDir/dist/index.html
